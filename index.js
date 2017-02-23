eca0629c5dc60616caf5b13b64c3d5ca5d3b5b26
shgtkshruch <shgtk.shruch@gmail.com>
2017-02-23 11:45:58 +0900

    Add menu

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 'use strict';
 
-const electron = require('electron');
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
+const {app, BrowserWindow, Menu, dialog} = require('electron');
 
 let mainWindow;
 
-app.on('ready', () => {
+let menuTemplate = [{
+  label: 'MyApp',
+  submenu: [
+    { label: 'About', accelerator: 'CmdOrCtrl+Shift+A', click() { showAboutDialog() }},
+    { type: 'separator' },
+    { label: 'Settings', accelerator: 'CmdOrCtrl+,', click() { showSettingsWindow() }},
+    { type: 'separator' },
+    { label: 'Quit', accelerator: 'CmdOrCtrl+Q', click() { app.quit() }},
+  ]
+}];
+
+let menu = Menu.buildFromTemplate(menuTemplate);
+
+function showAboutDialog() {
+  dialog.showMessageBox({
+    type: 'info',
+    buttons: ['OK'],
+    message: 'About This App',
+    detail: 'This app was created by @shgtkshruch'
+  });
+}
+
+function createMainWindow() {
+  // diplay menu
+  Menu.setApplicationMenu(menu);
+
   mainWindow = new BrowserWindow({
     width: 600,
     height: 400
@@ -19,4 +42,20 @@ app.on('ready', () => {
   mainWindow.on('closed', () => {
     mainWindow = null;
   });
+}
+
+app.on('ready', () => {
+  createMainWindow();
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('avtivate', () => {
+  if (mainWindow === null) {
+    createMainWindow();
+  }
 });
